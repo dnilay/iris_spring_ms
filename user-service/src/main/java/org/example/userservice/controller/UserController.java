a7b42c85977bc2de943194c3d146e4427aed09ca
nilay <mailnilaydas@gmail.com>
2020-06-07 16:39:08 +0530

    commit

--- user-service/src/main/java/org/example/userservice/controller/UserController.java
+++ user-service/src/main/java/org/example/userservice/controller/UserController.java
@@ -1,7 +1,6 @@
 package org.example.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
