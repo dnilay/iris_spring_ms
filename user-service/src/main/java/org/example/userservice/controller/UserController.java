c5ef3a362f6b13556d457d902fbf0c902975b3f9
nilay <mailnilaydas@gmail.com>
2020-06-07 15:24:20 +0530

    commit

--- user-service/src/main/java/org/example/userservice/controller/UserController.java
+++ user-service/src/main/java/org/example/userservice/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.example.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    @GetMapping("/status/check")
+	private Environment env;
+	@Autowired
+    public UserController(Environment env) {
+		super();
+		this.env = env;
+	}
+
+	@GetMapping("/status/check")
+
     public String check()
     {
-        return "user service working";
+        return "user service working: "+env.getProperty("local.server.port");
     }
 }
