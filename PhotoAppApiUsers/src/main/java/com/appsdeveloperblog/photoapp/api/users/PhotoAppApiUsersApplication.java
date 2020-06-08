d20855102bdaa74e5e45c64ff1d35fed752705e5
nilay <mailnilaydas@gmail.com>
2020-06-08 10:44:37 +0530

    commit

---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,31 @@
+package com.appsdeveloperblog.photoapp.api.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableFeignClients
+//@EnableCircuitBreaker
+public class PhotoAppApiUsersApplication {
+
+	@Autowired
+	Environment environment;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+}
---
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,99 @@
+package com.appsdeveloperblog.photoapp.api.users.service;
+
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
+import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+	
+	UsersRepository usersRepository;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	//RestTemplate restTemplate;
+	Environment environment;
+	//AlbumsServiceClient albumsServiceClient;
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository, 
+			BCryptPasswordEncoder bCryptPasswordEncoder,
+			//AlbumsServiceClient albumsServiceClient,
+			Environment environment)
+	{
+		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+		//this.albumsServiceClient = albumsServiceClient;
+		this.environment = environment;
+	}
+ 
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		
+		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
+		
+		ModelMapper modelMapper = new ModelMapper(); 
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+
+		usersRepository.save(userEntity);
+		
+		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+ 
+		return returnValue;
+	}
+
+	@Override
+	public UserDto getUserDetailsByEmail(String email) { 
+		UserEntity userEntity = usersRepository.findByEmail(email);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(email);
+		
+		
+		return new ModelMapper().map(userEntity, UserDto.class);
+	}
+
+	@Override
+	public UserDto getUserByUserId(String userId) {
+		
+        UserEntity userEntity = usersRepository.findByUserId(userId);     
+        if(userEntity == null) throw new UsernameNotFoundException("User not found");
+        
+        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
+        
+        /*
+        String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
+        
+        ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate.exchange(albumsUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
+        });
+        List<AlbumResponseModel> albumsList = albumsListResponse.getBody(); 
+        */
+        
+        logger.info("Before calling albums Microservice");
+ //       List<AlbumResponseModel> albumsList = albumsServiceClient.getAlbums(userId);
+        logger.info("After calling albums Microservice");
+        
+	//	userDto.setAlbums(albumsList);
+		
+		return userDto;
+	}
+	
+	
+	
+
+}
